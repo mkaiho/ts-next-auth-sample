7571164c49be8d67018183734cf89b87bba96130
mkaiho <mkaiho.cgengo@gmail.com>
2023-03-26 16:37:30 +0000

    modify baseUrl in tsconfig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
-import theme from '../theme'
+import theme from '@/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
