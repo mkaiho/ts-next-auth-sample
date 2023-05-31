dd7a148d7abac2c467f26ae39384cd86d7e6821a
mkaiho <mkaiho.cgengo@gmail.com>
2023-05-31 13:12:44 +0000

    format

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,9 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import { CssBaseline, ThemeProvider } from '@mui/material'
+
 import type { AppProps } from 'next/app'
+
 import theme from '@/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
